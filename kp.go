5f1f57d625e632dac747a7fe837985980ea7c3f2
Mathew Odden <locke105@gmail.com>
2020-08-04 20:57:29 -0500

    Cleanup examples and docs
    Remove old examples and add new examples in godoc format.
    These get compiler check on test run, so should be less
    fragile and will show up better in the go docs.

--- kp.go
+++ kp.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// keyprotect-go-client is a Go client library for interacting with the IBM KeyProtect service.
 package kp
 
 import (
