60b3e3c0c9df86d6d14ddaeff485fee475da79a2
Spurthi Ganapathineedi <gspurthi@Spurthis-MacBook-Pro-3.local>
2020-09-01 13:28:25 -0500

    Updated SDK with allowed IP instance policy

--- instances.go
+++ instances.go
@@ -26,6 +26,9 @@ const (
 
 	//AllowedNetwork defines the policy type as allowed network
 	AllowedNetwork = "allowedNetwork"
+
+	//AllowedIP defines the polity type as allowed ip that are whitelisted
+	AllowedIP = "allowedIP"
 )
 
 // InstancePolicy represents a instance-level policy of a key as returned by the KP API.
@@ -47,7 +50,8 @@ type PolicyData struct {
 
 // Attributes contains the detals of allowed network policy type
 type Attributes struct {
-	AllowedNetwork string `json:"allowed_network,omitempty"`
+	AllowedNetwork string   `json:"allowed_network,omitempty"`
+	AllowedIP      []string `json:"allowed_ip, omitempty"`
 }
 
 // InstancePolicies represents a collection of Policies associated with Key Protect instances.
@@ -80,6 +84,24 @@ func (c *Client) GetAllowedNetworkInstancePolicy(ctx context.Context) (*Instance
 	policyResponse := InstancePolicies{}
 
 	err := c.getInstancePolicy(ctx, AllowedNetwork, &policyResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(policyResponse.Policies) == 0 {
+		return nil, nil
+	}
+
+	return &policyResponse.Policies[0], nil
+}
+
+// GetAllowedIPInstancePolicy retrieves the allowed IP instance policy details associated with the instance.
+// For more information can refer the Key Protect docs in the link below:
+//<KP Link>
+func (c *Client) GetAllowedIPInstancePolicy(ctx context.Context) (*InstancePolicy, error) {
+	policyResponse := InstancePolicies{}
+
+	err := c.getInstancePolicy(ctx, AllowedIP, &policyResponse)
 	if err != nil {
 		return nil, err
 	}
@@ -163,6 +185,36 @@ func (c *Client) SetDualAuthInstancePolicy(ctx context.Context, enable bool) err
 	}
 
 	err := c.setInstancePolicy(ctx, DualAuthDelete, policyRequest)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+// SetAllowedIPInstancePolices updates the allowed IP instance policy details associated with an instance.
+// For more information can refet to the Key Protect docs in the link below:
+// <KP link>
+func (c *Client) SetAllowedIPInstancePolicy(ctx context.Context, enable bool, allowedIPs []string) error {
+	policy := InstancePolicy{
+		PolicyType: AllowedIP,
+		PolicyData: PolicyData{
+			Enabled:    &enable,
+			Attributes: &Attributes{},
+		},
+	}
+	if len(allowedIPs) != 0 {
+		policy.PolicyData.Attributes.AllowedIP = allowedIPs
+	}
+
+	policyRequest := InstancePolicies{
+		Metadata: PoliciesMetadata{
+			CollectionType:   policyType,
+			NumberOfPolicies: 1,
+		},
+		Policies: []InstancePolicy{policy},
+	}
+	err := c.setInstancePolicy(ctx, AllowedIP, policyRequest)
 	if err != nil {
 		return err
 	}
