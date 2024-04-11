603c5c2a52b8d0b65bd2f4dd0c7696267a96337d
Michael Darmawan <michael.darmawan@ibm.com>
2024-04-11 11:13:01 -0500

    remove commented code
    Signed-off-by: Michael Darmawan <michael.darmawan@ibm.com>

--- kmip_mgmt_adapters.go
+++ kmip_mgmt_adapters.go
@@ -82,7 +82,6 @@ type ListKmipAdaptersOptions struct {
 	Limit      *uint32
 	Offset     *uint32
 	TotalCount *bool
-	// CrkID      *string
 }
 
 // GetKMIPAdapters method lists KMIP Adapters associated with a specific KP instance.
@@ -104,9 +103,6 @@ func (c *Client) GetKMIPAdapters(ctx context.Context, listOpts *ListKmipAdapters
 		if listOpts.TotalCount != nil {
 			values.Set("totalCount", fmt.Sprint(*listOpts.TotalCount))
 		}
-		// if listOpts.CrkID != nil {
-		// 	values.Set("crk_id", *listOpts.CrkID)
-		// }
 		req.URL.RawQuery = values.Encode()
 	}
 
